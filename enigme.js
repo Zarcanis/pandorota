f17c1de5966af5e4a946b4b28808e55546f34f07
Zarcanis <125974657+Zarcanis@users.noreply.github.com>
2023-02-21 13:40:40 +0100

    Create enigme.js

---
+++ enigme.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+const passwordInput = document.querySelector('#password');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const password = passwordInput.value.toLowerCase();
+  
+  if (password === 'homme') {
+    window.location.href = 'lettre.html';
+  } else {
+    alert('Réponse incorrecte. Réessayez');
+  }
+});
