d2ceefd50630770e82a44a8353fc2175309c6b76
Zarcanis <125974657+Zarcanis@users.noreply.github.com>
2023-02-22 17:16:53 +0100

    Update scriptmosaic.js

--- scriptmosaic.js
+++ scriptmosaic.js
@@ -1,13 +1,21 @@
-function checkAnswer(image) {
- var message = document.getElementById("message");
- if (image.alt === "image 7") {
-  message.innerHTML = "Bravo, c'est la bonne réponse !";
-  setTimeout(function() {
-   window.location.href = "lettre.html";
-  }, 2000);
- } else {
-  message.innerHTML = "Désolé, ce n'est pas la bonne réponse. Veuillez réessayer.");
-  }
-}
+// Sélectionnez tous les éléments de l'image avec la classe "puzzle-piece"
+const puzzlePieces = document.querySelectorAll('.puzzle-piece');
 
-
+// Bouclez à travers chaque élément d'image et ajoutez un gestionnaire d'événements de clic
+puzzlePieces.forEach(piece => {
+  piece.addEventListener('click', () => {
+    // Vérifiez si l'image cliquée a la bonne réponse
+    if (piece.alt === 'image7') {
+      // Affichez un message de félicitations et redirigez vers la page suivante après 2 secondes
+      const message = document.getElementById('message');
+      message.textContent = 'Bravo, c\'est la bonne réponse !';
+      setTimeout(() => {
+        window.location.href = 'lettree.html';
+      }, 2000);
+    } else {
+      // Affichez un message d'erreur si la réponse est incorrecte
+      const message = document.getElementById('message');
+      message.textContent = 'Désolé, ce n\'est pas la bonne réponse. Essayez encore !';
+    }
+  });
+})
