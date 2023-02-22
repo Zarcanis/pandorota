8624134652b134c1d4794a050da81607e41576bf
Zarcanis <125974657+Zarcanis@users.noreply.github.com>
2023-02-22 16:42:34 +0100

    Update scriptpage4.js

--- scriptpage4.js
+++ scriptpage4.js
@@ -1,8 +1,13 @@
-function checkAnswer() {
-  var userAnswer = document.getElementById("user-answer").value.toLowerCase();
-  if (userAnswer === "votre réponse") {
-    window.location.href = "page-suivante.html"; //remplacer "page-suivante.html" par le nom de la page que vous souhaitez rediriger
-  } else {
-    alert("Désolé, mauvaise réponse. Veuillez réessayer.");
+function checkAnswer(image) {
+ var message = document.getElementById("message");
+ if (image.alt === "image 7") {
+  message.innerHTML = "Bravo, c'est la bonne réponse !";
+  setTimeout(function() {
+   window.location.href = "lettre.html";
+  }, 2000);
+ } else {
+  message.innerHTML = "Désolé, ce n'est pas la bonne réponse. Veuillez réessayer.");
   }
 }
+
+
