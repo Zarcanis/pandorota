6b229edd6e87e634da8690e95b23922f60865ddf
Zarcanis <125974657+Zarcanis@users.noreply.github.com>
2023-02-23 17:32:57 +0100

    Update scriptmosaic2.js

--- scriptmosaic2.js
+++ scriptmosaic2.js
@@ -4,7 +4,7 @@ var message = document.getElementById("message");
 for (var i = 0; i < options.length; i++) {
 	options[i].addEventListener("click", function() {
 		if (this.alt === "image true") {
-			message.innerHTML = "Hey, well done! :)";
+			message.innerHTML = "See ? Being different is the key";
 			setTimeout(function() {
 				window.location.href = "flambeau.html";
 			}, 2000);
