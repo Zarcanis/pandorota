22d4b1f106d041e9c6de383219d3f58144f5ebc2
Zarcanis <125974657+Zarcanis@users.noreply.github.com>
2023-02-23 17:27:43 +0100

    Update scriptmosaic2.js

--- scriptmosaic2.js
+++ scriptmosaic2.js
@@ -4,12 +4,12 @@ var message = document.getElementById("message");
 for (var i = 0; i < options.length; i++) {
 	options[i].addEventListener("click", function() {
 		if (this.alt === "image 7") {
-			message.innerHTML = "Bravo, c'est la bonne réponse !";
+			message.innerHTML = "Hey, well done! :)";
 			setTimeout(function() {
-				window.location.href = "lettre.html";
+				window.location.href = "flambeau.html";
 			}, 2000);
 		} else {
-			message.innerHTML = "Désolé, ce n'est pas la bonne réponse. Essayez encore.";
+			message.innerHTML = "What are you doing, clicking at random?!";
 		}
 	});
 }
